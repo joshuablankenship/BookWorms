dba88d2481931e5685e7a384faa0496d928c3464
alecgilchrist <alecgilchrist@gmail.com>
2018-07-23 18:28:03 -0500

    basic setup of react

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+// const items = require('../database-mongo');
+
+const app = express();
+
+
+ app.use(express.static(__dirname + '/../client/dist'));
+
+
+app.get('/items', (req, res) => {
+    items.selectAll((err, data) => {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000!');
+});

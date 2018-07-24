3c81e7c94c419147f2d131f35cd1098e628690e4
alecgilchrist <alecgilchrist@gmail.com>
2018-07-24 13:09:32 -0500

    Rating and Description body coming from goodreads API call

--- server/index.js
+++ server/index.js
@@ -10,11 +10,15 @@ const app = express();
 
  app.use(express.static(__dirname + '/../client/dist'));
 
-
+// this is the average rating pulled from the HTML data.data.split('<average_rating>')[1].slice(0, 4)
+// this will pull description from gooReads 90% data.data.split('<description>')[1].split(']')[0].slice(9)
 app.get('/title', (req, res) => {
     // let title = req.body.title;
-    helpers.goodReadsData('Where The Wild Things Are')
-    .then(data => console.log(data.data))
+    helpers.goodReadsData('Green Eggs and Ham')
+    .then(data => {
+        console.log(data.data.split('<average_rating>')[1].slice(0, 4))
+        console.log(data.data.split('<description>')[1].split(']')[0].slice(9))
+    })
     .catch(err => console.log(err));
    
 });

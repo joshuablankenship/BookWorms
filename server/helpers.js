28a6b58f268d13b5028420c3077a547e197dcf63
alecgilchrist <alecgilchrist@gmail.com>
2018-07-24 11:11:41 -0500

    adding get method for goodReads

--- server/helpers.js
+++ server/helpers.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
 const config = require('./config.js');
 const libThing = config.libThing
+const goodReads = config.goodReads
 
-
-const getISBNbyTitle = () => {
-    // return axios.get()
+// Finally got a response thats useable from good reads!!! format below
+//  https://www.goodreads.com/book/title.xml?&key=API_KEY&title=where+the+wild+things+are
+const goodReadsData = (title) => {
+    let formattedTitle = title.split(' ').join('+');
+    return axios.get(`https://www.goodreads.com/book/title.xml?&${goodReads}&${formattedTitle}`)
 }

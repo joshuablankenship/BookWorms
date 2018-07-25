3fcee415a58fda63a8e7d5b24b8ffee781d36f21
alecgilchrist <alecgilchrist@gmail.com>
2018-07-24 19:04:47 -0500

    google and goodreads basic API call working. config set up

--- server/helpers.js
+++ server/helpers.js
@@ -1,17 +1,18 @@
 const axios = require('axios');
-// const config = require('../config.js');
-// const goodReads = config.goodReads
+const config = require('../config.js');
+const goodReads = config.goodReads
+const google = config.google
 
 // Finally got a response thats useable from good reads!!! format below
 //  https://www.goodreads.com/book/title.xml?&key=API_KEY&title=where+the+wild+things+are
 const goodReadsData = (title) => {
     let formattedTitle = title.split(' ').join('+');
-    // return axios.get(`https://www.goodreads.com/book/title.xml?&key=${goodReads}&title=${formattedTitle}`)
+    return axios.get(`https://www.goodreads.com/book/title.xml?&key=${goodReads}&title=${formattedTitle}`)
 }
 
 const googleBooks = (title) => {
-    //  let formattedTitle = title.split(' ').join('_');
-     return axios.get(`https://www.googleapis.com/books/v1/volumes?q=where_the_wild_things_are&key=AIzaSyCi1Fgi8K1bnX8CVNO1ZSs0w9JPzS1NXbg`)
+     let format = title.split(' ').join('_');
+     return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${format}&key=${google}`)
 }
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -10,9 +10,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
  
-
+// res.data.items[0] will access the first book on search of a title. with a proper title this works well.
 app.get('/googleData', (req, res) => {
-    helpers.googleBooks()
+    helpers.googleBooks('Sherlock Holmes and the hounds of baskerville')
     .then((res) => console.log(res.data.items[0]))
     .catch((err) => console.log(err))
 
@@ -20,14 +20,14 @@ app.get('/googleData', (req, res) => {
 
 // this is the average rating pulled from the HTML data.data.split('<average_rating>')[1].slice(0, 4)
 // this will pull description from gooReads 90% data.data.split('<description>')[1].split(']')[0].slice(9)
-app.get('/title', (req, res) => {
+app.get('/goodreads', (req, res) => {
     // let title = req.body.title;
-    // helpers.goodReadsData('Green Eggs and Ham')
-    // .then(data => {
-    //     console.log(data.data.split('<average_rating>')[1].slice(0, 4))
-    //     console.log(data.data.split('<description>')[1].split(']')[0].slice(9))
-    // })
-    // .catch(err => console.log(err));
+    helpers.goodReadsData('Green Eggs and Ham')
+    .then(data => {
+        console.log(data.data.split('<average_rating>')[1].slice(0, 4))
+        console.log(data.data.split('<description>')[1].split(']')[0].slice(9))
+    })
+    .catch(err => console.log(err));
    
 });
 

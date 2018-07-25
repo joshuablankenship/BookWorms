70f2c4e6545f78cbac920c68a45be7e1011240ea
Josh <joshuablankenshipnola@gmail.com>
2018-07-25 18:38:39 -0500

    added routes and Nav

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,11 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
+import Nav from './components/Nav.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import Main from './components/Main.jsx';
-
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	Redirect,
+	withRouter
+} from 'react-router-dom'
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -33,8 +39,15 @@ class App extends React.Component {
     return (
       <div>
         {/* <h1>Bookworms</h1> */}
-        
-        <Main />
+        <Router>
+		<div>
+			
+      <Route path="/" component={Nav}/>
+      <Route path="/main" component={Main}/>
+			<Route path="/login" component={Login}/>
+			<Route path="/signup" component={Signup}/>
+		</div>
+    </Router>
       </div>
       
     );

6d1d2a329b462c69d8b7fd2b827859fee3665ea7
Paul Marinaro <paulmarinaro@gmail.com>
2018-07-25 21:53:36 -0500

    installed react-router-dom, moved mockData

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,18 +2,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
+import Nav from './components/Nav.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import Main from './components/Main.jsx';
-import DATA from '../../mockData.js'
-
+import DATA from './mockData';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom'
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
-    
+      items: [],
     };
   }
   
@@ -36,11 +41,16 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <h1>Bookworms</h1> */}
-        <Main items={this.state.items} />
-        {/* <Main /> */}
+        <Router>
+          <div>
+            <Route path="/" component={Nav} />
+            <Route path="/main" component={Main} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </div>
+        </Router>
       </div>
-      
+
     );
   }
 }

86b2a12102041873fb2859aa3327156e772c25a8
alecgilchrist <alecgilchrist@gmail.com>
2018-07-25 22:15:15 -0500

    tyle cleanup after pull

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,17 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import Nav from './components/Nav.jsx';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter,
+} from 'react-router-dom';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import Main from './components/Main.jsx';
-import {
-	BrowserRouter as Router,
-	Route,
-	Link,
-	Redirect,
-	withRouter
-} from 'react-router-dom'
+import Nav from './components/Nav.jsx';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -39,15 +40,15 @@ class App extends React.Component {
     return (
       <div>
         <Router>
-		<div>
-      <Route path="/" component={Nav}/>
-      <Route path="/main" component={Main}/>
-			<Route path="/login" component={Login}/>
-			<Route path="/signup" component={Signup}/>
-		</div>
-    </Router>
+          <div>
+            <Route path="/" component={Nav} />
+            <Route path="/main" component={Main} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </div>
+        </Router>
       </div>
-      
+
     );
   }
 }

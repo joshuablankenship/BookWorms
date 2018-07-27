8a7224b2e8d13f8d51b0f78f21923d5c1ef269e6
Josh <joshuablankenshipnola@gmail.com>
2018-07-27 18:34:30 -0500

    merge

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,7 +14,6 @@ import {
 import Nav from './components/Nav.jsx';
 import DATA from './mockData';
 import REVIEWS from './mockReview';
-
 import HomePage from './components/HomePage.jsx';
 import LoginPage from './containers/LoginPage.jsx';
 import Logout from './containers/Logout.jsx';
@@ -63,30 +62,6 @@ class App extends Component {
     super(props);
     this.state = {
       items: [],
-<<<<<<< HEAD
-      authenticated: false
-    }
-  
-  this.searchForBook = (title) => {
-    console.log(title, 'query in index.jsx');
-    axios.get('/googleData', {
-      params: { title },
-    })
-      .then((response) => {
-        this.setState({ items: [response.data] });
-      })
-      .catch((error) => {
-        console.error(error, 'error in index.jsx');
-      });
-  };
-}
-  componentDidMount() {
-    // check if user is logged in on refresh
-    this.toggleAuthenticateStatus()
-        
-    };
-  
-=======
       reviews: [],
       reviewToggled: false,
     };
@@ -108,19 +83,23 @@ class App extends Component {
         // if no item is passed in, set reviewToggled to false to revert to MainList view when searching
         this.setState({ reviewToggled: false });
       }
-    }
+    };
   }
->>>>>>> c5ae00fc1dc1678840c2d5ff9e789d507b82467b
-
-  toggleAuthenticateStatus() {
+  componentDidMount(){
+    // check if user is logged in on refresh
+    this.toggleAuthenticateStatus();
     this.setState({
       items: DATA,
       reviews: REVIEWS
     });
+  }
+
+  toggleAuthenticateStatus() {
+    
     // check authenticated status and toggle state based on that
     this.setState({ authenticated: Auth.isUserAuthenticated() })
   }
-
+  
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
@@ -128,13 +107,9 @@ class App extends Component {
           <div>
         <Route
               path="/"
-              render={props => <Nav {...props} items={this.state.items} reviews={this.state.reviews}
-                reviewToggle={this.reviewToggle.bind(this)}
-                reviewToggled={this.state.reviewToggled}
-                handleSearchInput={this.searchForBook.bind(this)} 
-                />}
+              render={props => <Nav {...props} items={this.state.items} 
+                handleSearchInput={this.searchForBook.bind(this)} />}
             />
-<<<<<<< HEAD
 
             
 
@@ -143,17 +118,6 @@ class App extends Component {
             <LoggedOutRoute path="/login" component={LoginPage} toggleAuthenticateStatus={() => this.toggleAuthenticateStatus()} />
             <LoggedOutRoute path="/signup" component={SignUpPage}/>
             <Route path="/logout" component={Logout} />
-=======
-            <Route
-              path="/main"
-              render={props => <Main {...props} items={this.state.items} reviews={this.state.reviews}
-                reviewToggle={this.reviewToggle.bind(this)} 
-                reviewToggled={this.state.reviewToggled}
-                handleSearchInput={this.searchForBook.bind(this)} />}
-            />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
->>>>>>> c5ae00fc1dc1678840c2d5ff9e789d507b82467b
           </div>
 
         </Router>

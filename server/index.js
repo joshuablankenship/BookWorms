9c5889eb44a6c852f02f3dd6bcc3458028561e83
alecgilchrist <alecgilchrist@gmail.com>
2018-07-29 02:43:33 -0500

    added sort method to DB pulled books before sending to server

--- server/index.js
+++ server/index.js
@@ -71,6 +71,19 @@ app.get('/topRated', (req, res) => {
           });
         }
       });
+      top.sort((a, b) => {
+        // Use toUpperCase() to ignore character casing
+        const ratingA = a.aggregateRating;
+        const ratingB = b.aggregateRating;
+
+        let comparison = 0;
+        if (ratingA < ratingB) {
+          comparison = 1;
+        } else if (ratingA > ratingB) {
+          comparison = -1;
+        }
+        return comparison;
+      });
       res.send({ len: top.length, top });
     }
   });
